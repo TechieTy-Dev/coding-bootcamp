28d19830916e69fc75bdbb082158258aeed1cc09
Tyler Smith <tysocialhq@gmail.com>
2023-05-02 10:38:06 -0500

    Fixing Font Size and Testimonial spacing

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
 							alt="tanya"
 							className=" w-[60%] mx-auto shadow-2xl rounded-md lg:w-[60%] lg:mt-12"
 						/>
-						<div className="flex bg-white w-[5.5rem] absolute left-[200px] bottom-4 p-2.5 rounded-full justify-between shadow-md   lg:bottom-[2.25rem] lg:left-[215px]">
+						<div className="flex bg-white w-[5.5rem] absolute left-[200px] bottom-4 p-2.5 rounded-full justify-between shadow-md   lg:bottom-[3rem] lg:left-[215px]">
 							{/* Prev Btn */}
 							<button onClick={displayJohn}>
 								<img
@@ -45,7 +45,7 @@ function App() {
 						</div>
 					</div>
 					{/* Testimonial */}
-					<div className=" mt-12 flex flex-col items-center justify-center lg:w-[80%] lg:text-left lg:items-start lg:mr-[-16rem] lg:pl-44 lg:z-10">
+					<div className=" mt-12 flex flex-col items-center justify-center lg:w-[80%] lg:text-left lg:items-start lg:mr-[-12rem] lg:pl-44 lg:z-10">
 						<img src={quotes} alt="quotes" className="w-24  lg:ml-24" />
 						<p className="text-darkblue font-light mt-[-3rem] w-[90%] tracking-wide lg:leading-[3.25rem] lg:w-[85%]">
 							{" "}
@@ -54,11 +54,17 @@ function App() {
 							in the job of my dreams and so excited about the future. ”
 						</p>
 						{/* User Details */}
-						<div className="mt-4 lg:text-left lg:flex lg:gap-2 lg:pt-8	">
+						<div className="mt-4 lg:text-left lg:flex lg:gap-3 lg:pt-8	">
 							{/* Name */}
-							<h1 className="font-bold text-darkblue"> Tanya Sinclair</h1>
+							<h1 className="font-bold text-darkblue text-2xl">
+								{" "}
+								Tanya Sinclair
+							</h1>
 							{/* Job Title */}
-							<span className="font-light text-lightblue"> UX Engineer</span>
+							<span className="font-light text-lightblue text-2xl">
+								{" "}
+								UX Engineer
+							</span>
 						</div>
 					</div>
 				</section>
@@ -76,7 +82,7 @@ function App() {
 							alt="john"
 							className=" w-[60%] mx-auto shadow-2xl rounded-md lg:w-[60%] lg:mt-12"
 						/>
-						<div className="flex bg-white w-[5.5rem] absolute left-[200px] bottom-4 p-2.5 rounded-full justify-between shadow-md   lg:bottom-[2.25rem] lg:left-[215px]">
+						<div className="flex bg-white w-[5.5rem] absolute left-[200px] bottom-4 p-2.5 rounded-full justify-between shadow-md   lg:bottom-[3rem] lg:left-[215px]">
 							{/* Prev Btn */}
 							<button onClick={displayTanya}>
 								<img
@@ -95,7 +101,7 @@ function App() {
 							</button>
 						</div>
 					</div>
-					<div className=" mt-12 flex flex-col items-center justify-center lg:w-[80%] lg:text-left lg:items-start lg:mr-[-16rem] lg:pl-44 lg:z-10">
+					<div className=" mt-12 flex flex-col items-center justify-center lg:w-[80%] lg:text-left lg:items-start lg:mr-[-12rem] lg:pl-44 lg:z-10">
 						<img src={quotes} alt="quotes" className="w-24  lg:ml-24" />
 						{/* Testimonial Quote */}
 						<p className="text-darkblue font-light mt-[-3rem] w-[90%] tracking-wide lg:leading-[3.25rem] lg:w-[85%]">
@@ -106,11 +112,14 @@ function App() {
 							professional developer. ”
 						</p>
 						{/* User Details */}
-						<div className="mt-4 lg:text-left lg:flex lg:gap-2 lg:pt-8	">
+						<div className="mt-4 lg:text-left lg:flex lg:gap-3 lg:pt-8	">
 							{/* Name */}
-							<h1 className="font-bold text-darkblue"> John Tarkpor</h1>
+							<h1 className="font-bold text-darkblue text-2xl">
+								{" "}
+								John Tarkpor
+							</h1>
 							{/* Job Title */}
-							<span className="font-light text-lightblue">
+							<span className="font-light text-lightblue text-2xl">
 								{" "}
 								Junior Front-end Developer
 							</span>
